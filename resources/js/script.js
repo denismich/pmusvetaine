b1dbfaf2a5753c54b54d2b3a26f05ac5b8b6031e
denismich <dmichailovskij@gmail.com>
2023-04-03 18:49:42 +0300

    Test

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,7 +12,7 @@ document.querySelector('#banner-registration-button').addEventListener('click', 
   top: document.querySelector('#registration-section').offsetTop,
   behavior: 'smooth'
 })
-} catch {
+} catch(error) {
   window.scrollTo(0, document.querySelector('#registration-section').offsetTop)
 }});
 
@@ -23,7 +23,7 @@ const fullScreenImage = document.querySelector('#full-screen-image');
 
 let currentImageIndex = 0;
 
-imageContainer.addEventListener('click', (event) => {
+imageContainer.addEventListener('click', function(event) {
   if (event.target.tagName === 'IMG') {
       currentImageIndex = Array.from(imageContainer.children).indexOf(event.target);
       fullScreenImage.src = event.target.src;
@@ -31,18 +31,18 @@ imageContainer.addEventListener('click', (event) => {
   }
 });
 
-fullScreenContainer.addEventListener('click', (event) => {
+fullScreenContainer.addEventListener('click', function(event) {
   if (event.target === fullScreenContainer) {
       fullScreenContainer.style.display = 'none';
   }
 });
 
-document.querySelector('#prev-button').addEventListener('click', () => {
+document.querySelector('#prev-button').addEventListener('click', function() {
   currentImageIndex = (currentImageIndex + imageContainer.children.length - 1) % imageContainer.children.length;
   fullScreenImage.src = imageContainer.children[currentImageIndex].src;
 });
 
-document.querySelector('#next-button').addEventListener('click', () => {
+document.querySelector('#next-button').addEventListener('click', function() {
   currentImageIndex = (currentImageIndex + 1) % imageContainer.children.length;
   fullScreenImage.src = imageContainer.children[currentImageIndex].src;
 });
@@ -55,7 +55,7 @@ let myTimer;
 
 const showSlides = n => {
   clearInterval(myTimer);
-  myTimer = setInterval(() => showSlides(slideIndex += 1), 4000);
+  myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000);
 
   const slides = document.getElementsByClassName('slide');
   
@@ -72,10 +72,10 @@ const showSlides = n => {
   dots[slideIndex-1].className += ' active';
 }
 
-const pause = () => clearInterval(myTimer);
-const resume = () => myTimer = setInterval(() => showSlides(slideIndex += 1), 4000);
+const pause = function() {clearInterval(myTimer)};
+const resume = function() {myTimer = setInterval(() => showSlides(slideIndex += 1), 4000)};
 
-window.addEventListener('load', () => {
+window.addEventListener('load', function() {
   showSlides(slideIndex);
 
   const slideshowContainer = document.querySelector('#slideshow-container');
@@ -85,7 +85,7 @@ window.addEventListener('load', () => {
 });
 
 for (let i = 0; i < dots.length; i++) {
-  dots[i].addEventListener('click', () => showSlides(slideIndex = i + 1));
+  dots[i].addEventListener('click', function() {showSlides(slideIndex = i + 1)});
 }
 
 //Registration form select dropdown
@@ -93,7 +93,7 @@ const select = document.querySelector('#procedure');
 const dropdown = document.querySelector('#custom-dropdown');
 const dropdownOptions = document.querySelectorAll('#custom-dropdown .option');
 
-const toggleDropdown = () => {
+const toggleDropdown = function() {
   dropdown.classList.toggle('opened');
 
   if (dropdown.classList.contains('opened')) {
@@ -109,17 +109,17 @@ const toggleDropdown = () => {
     dropdown.style.top = ((spaceBelow >= dropdownRect.height || spaceAbove < dropdownRect.height) ? distanceDownward : distanceUpward) + 'px';
   }
 }
-const closeDropdownFromOutside = event => event.target !== select && dropdown.classList.remove('opened');
+const closeDropdownFromOutside = function(event) {event.target !== select && dropdown.classList.remove('opened')};
 
-const selectOption = event => select.value = event.currentTarget.textContent;
+const selectOption = function(event) {select.value = event.currentTarget.textContent};
 
-select.addEventListener('mousedown', event => event.preventDefault());
+select.addEventListener('mousedown', function(event) {event.preventDefault()});
 
 select.addEventListener('click', toggleDropdown);
 
 document.body.addEventListener('click', closeDropdownFromOutside);
 
-dropdownOptions.forEach(option => option.addEventListener('click', selectOption));
+dropdownOptions.forEach(function(option) {option.addEventListener('click', selectOption)});
 
 //Email sending on registration
 const form = document.querySelector('#registration-form');
@@ -129,7 +129,7 @@ const errorModal = document.querySelector('#error-modal');
 
 const modalCloseButtons = document.querySelectorAll('.modal-close-button');
 
-const sendEmail = () => {
+const sendEmail = function() {
   const name = document.querySelector('#name').value;
   const email = document.querySelector('#email').value;
   const phone = document.querySelector('#phone').value;
@@ -145,39 +145,39 @@ const sendEmail = () => {
       From : 'dmichailovskij@gmail.com',
       Subject : 'Registracijos Procedūrai Užklausa',
       Body : notification
-  }).then(message => {
+  }).then(function(message) {
     if (message === 'OK') {
       successModal.style.display = 'flex';
     } else {
       throw new Error('Email was not sent');
     }
   })
-  .catch(error => {
+  .catch(function(error) {
     errorModal.style.display = 'flex';
   });
 }
 
-const closeModal = () => {
+const closeModal = function() {
   successModal.style.display = 'none';
   errorModal.style.display = 'none';
 }
 
-const closeModalFromOutside = event => {
+const closeModalFromOutside = function(event) {
   if (event.target === successModal || event.target === errorModal) {
     closeModal();
   }
 }
 
-modalCloseButtons.forEach(button => button.addEventListener('click', closeModal));
+modalCloseButtons.forEach(function(button) {button.addEventListener('click', closeModal)});
 
 document.body.addEventListener('click', closeModalFromOutside);
 
 //Button for scrolling to top
 const scrollToTopButton = document.querySelector('#scroll-to-top-button');
 
-window.addEventListener("scroll", () => window.scrollY > 1000 ? scrollToTopButton.classList.add("show") : scrollToTopButton.classList.remove("show"));
+window.addEventListener("scroll", function() {window.scrollY > 1000 ? scrollToTopButton.classList.add("show") : scrollToTopButton.classList.remove("show")});
 
-scrollToTopButton.addEventListener('click', () => window.scrollTo({
+scrollToTopButton.addEventListener('click', function() {window.scrollTo({
   top: 0,
   behavior: 'smooth'
-}));
+})});
