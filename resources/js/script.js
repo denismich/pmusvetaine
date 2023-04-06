b5546c5874f9cfccba225efb59cc4fb5af7c553d
denismich <dmichailovskij@gmail.com>
2023-04-06 10:21:46 +0300

    Test

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,7 +4,7 @@
 var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 //Header navigation bar icon
-var headerNavIcon = document.querySelector('#header-nav-icon');
+const headerNavIcon = document.querySelector('#header-nav-icon');
 headerNavIcon.addEventListener('click', function() {
   document.querySelector('#header-nav-menu').classList.toggle('visible');
   headerNavIcon.classList.toggle('open');
@@ -22,15 +22,15 @@ document.querySelector('#banner-registration-button').addEventListener('click', 
 }});
 
 //Gallery images enlarging
-var imageContainer = document.querySelector('#gallery-photos-container');
-var fullScreenContainer = document.querySelector('#full-screen-container');
-var fullScreenImage = document.querySelector('#full-screen-image');
-var imageArray = [];
-for(var i = 0; i < imageContainer.children.length; i++) {
+const imageContainer = document.querySelector('#gallery-photos-container');
+const fullScreenContainer = document.querySelector('#full-screen-container');
+const fullScreenImage = document.querySelector('#full-screen-image');
+const imageArray = [];
+for(let i = 0; i < imageContainer.children.length; i++) {
   imageArray.push(imageContainer.children[i]);
 }
 
-var currentImageIndex = 0;
+let currentImageIndex = 0;
 
 imageContainer.addEventListener('click', function(event) {
   if (event.target.tagName === 'IMG') {
@@ -57,21 +57,21 @@ document.querySelector('#next-button').addEventListener('click', function() {
 });
 
 //Testimonials slides
-var dots = document.getElementsByClassName('dot');
+const dots = document.getElementsByClassName('dot');
 
-var slideIndex = 1;
-var myTimer;
+let slideIndex = 1;
+let myTimer;
 
-var showSlides = function(n) {
+const showSlides = function(n) {
   clearInterval(myTimer);
   myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000);
 
-  var slides = document.getElementsByClassName('slide');
+  const slides = document.getElementsByClassName('slide');
   
   n > slides.length && (slideIndex = 1);
   n < 1 && (slideIndex = slides.length);
 
-  for (var i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
       dots[i].classList.remove('active');
   }
@@ -80,46 +80,46 @@ var showSlides = function(n) {
   dots[slideIndex-1].className += ' active';
 }
 
-var pause = function() {clearInterval(myTimer)};
-var resume = function() {myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000)};
+const pause = function() {clearInterval(myTimer)};
+const resume = function() {myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000)};
 
 window.addEventListener('load', function() {
   showSlides(slideIndex);
 
-  var slideshowContainer = document.querySelector('#slideshow-container');
+  const slideshowContainer = document.querySelector('#slideshow-container');
 
   slideshowContainer.addEventListener('mouseenter', pause)
   slideshowContainer.addEventListener('mouseleave', resume)
 });
 
-for (var i = 0; i < dots.length; i++) {
+for (let i = 0; i < dots.length; i++) {
   dots[i].addEventListener('click', function() {showSlides(slideIndex = i + 1)});
 }
 
 //Registration form select dropdown
-var select = document.querySelector('#procedure');
-var dropdown = document.querySelector('#custom-dropdown');
-var dropdownOptions = document.querySelectorAll('#custom-dropdown .option');
+const select = document.querySelector('#procedure');
+const dropdown = document.querySelector('#custom-dropdown');
+const dropdownOptions = document.querySelectorAll('#custom-dropdown .option');
 
-var toggleDropdown = function() {
+const toggleDropdown = function() {
   dropdown.classList.toggle('opened');
 
   if (dropdown.classList.contains('opened')) {
     dropdown.style.top = '0';
     
-    var selectRect = select.getBoundingClientRect();
-    var dropdownRect = dropdown.getBoundingClientRect();
-    var spaceBelow = document.documentElement.clientHeight - selectRect.bottom;
-    var spaceAbove = selectRect.top;
-    var distanceDownward = selectRect.bottom - dropdownRect.top;
-    var distanceUpward = selectRect.top - dropdownRect.bottom;
+    const selectRect = select.getBoundingClientRect();
+    const dropdownRect = dropdown.getBoundingClientRect();
+    const spaceBelow = document.documentElement.clientHeight - selectRect.bottom;
+    const spaceAbove = selectRect.top;
+    const distanceDownward = selectRect.bottom - dropdownRect.top;
+    const distanceUpward = selectRect.top - dropdownRect.bottom;
     
     dropdown.style.top = ((spaceBelow >= dropdownRect.height || spaceAbove < dropdownRect.height) ? distanceDownward : distanceUpward) + 'px';
   }
 }
-var closeDropdownFromOutside = function(event) {event.target !== select && dropdown.classList.remove('opened')};
+const closeDropdownFromOutside = function(event) {event.target !== select && dropdown.classList.remove('opened')};
 
-var selectOption = function(event) {select.value = event.currentTarget.textContent};
+const selectOption = function(event) {select.value = event.currentTarget.textContent};
 
 select.addEventListener('mousedown', function(event) {event.preventDefault()});
 
@@ -127,26 +127,26 @@ select.addEventListener('click', toggleDropdown);
 
 document.body.addEventListener('click', closeDropdownFromOutside);
 
-for(var i = 0; i < dropdownOptions.length; i++) {
+for(let i = 0; i < dropdownOptions.length; i++) {
   dropdownOptions[i].addEventListener('click', selectOption);
 }
 
 //Email sending on registration
-var form = document.querySelector('#registration-form');
+const form = document.querySelector('#registration-form');
 
-var successModal = document.querySelector('#success-modal');
-var errorModal = document.querySelector('#error-modal');
+const successModal = document.querySelector('#success-modal');
+const errorModal = document.querySelector('#error-modal');
 
-var modalCloseButtons = document.querySelectorAll('.modal-close-button');
+const modalCloseButtons = document.querySelectorAll('.modal-close-button');
 
-var sendEmail = function() {
-  var name = document.querySelector('#name').value;
-  var email = document.querySelector('#email').value;
-  var phone = document.querySelector('#phone').value;
-  var visitDate = document.querySelector('#date').value;
-  var procedure = document.querySelector('#procedure').value;
-  var message = document.querySelector('#message').value;
-  var notification = `Registracijos procedūrai užklausa: <br><br>Vardas:  ${name} <br>El. paštas:  ${email} <br>Telefonas:  ${phone} <br>Data:  ${visitDate} <br>
+const sendEmail = function() {
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const phone = document.querySelector('#phone').value;
+  const visitDate = document.querySelector('#date').value;
+  const procedure = document.querySelector('#procedure').value;
+  const message = document.querySelector('#message').value;
+  const notification = `Registracijos procedūrai užklausa: <br><br>Vardas:  ${name} <br>El. paštas:  ${email} <br>Telefonas:  ${phone} <br>Data:  ${visitDate} <br>
     Procedūra:  ${procedure} <br>Žinutė:  ${message}`;
 
   Email.send({
@@ -167,25 +167,25 @@ var sendEmail = function() {
   });
 }
 
-var closeModal = function() {
+const closeModal = function() {
   successModal.style.display = 'none';
   errorModal.style.display = 'none';
 }
 
-var closeModalFromOutside = function(event) {
+const closeModalFromOutside = function(event) {
   if (event.target === successModal || event.target === errorModal) {
     closeModal();
   }
 }
 
-for(var i = 0; i < modalCloseButtons.length; i++) {
+for(let i = 0; i < modalCloseButtons.length; i++) {
   modalCloseButtons[i].addEventListener('click', closeModal);
 }
 
 document.body.addEventListener('click', closeModalFromOutside);
 
 //Button for scrolling to top
-var scrollToTopButton = document.querySelector('#scroll-to-top-button');
+const scrollToTopButton = document.querySelector('#scroll-to-top-button');
 
 window.addEventListener("scroll", function() {window.scrollY > 1000 ? scrollToTopButton.classList.add("show") : scrollToTopButton.classList.remove("show")});
 
