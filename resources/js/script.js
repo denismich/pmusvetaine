5feb5a3d3fe9935aac97d743718514230b210921
denismich <dmichailovskij@gmail.com>
2023-04-07 13:31:44 +0300

    Testing2

--- resources/js/script.js
+++ resources/js/script.js
@@ -148,7 +148,7 @@ var sendEmail = function() {
   var visitDate = document.querySelector('#date').value;
   var procedure = document.querySelector('#procedure').value;
   var message = document.querySelector('#message').value;
-  var notification = `Registracijos procedūrai užklausa: <br><br>Vardas:  `;
+  var notification = 'Registracijos procedūrai užklausa: <br><br>Vardas:  ';
 
   Email.send({
       SecureToken: '90677d85-8654-4537-91d0-eacde4cbff0d',
