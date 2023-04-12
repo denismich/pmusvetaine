abee172242153fd88681af6b51a7a2cebf7a7ab4
denismich <dmichailovskij@gmail.com>
2023-04-12 20:49:08 +0300

    Testing

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,20 +24,22 @@ ajax: function (e, n) {
     null != n && n(e)
   }, t.send()
 },
-createCORSRequest: function (method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // Most browsers.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // IE8 & IE9
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
+createCORSRequest: function (e, n) {
+  var t = new XMLHttpRequest;
+
+  // add this line
+  t._restrictedHeaders = {};
+
+  if ("withCredentials" in t) {
+    t.open(e, n, !0);
+  } else if ("undefined" != typeof XDomainRequest) {
+    t = new XDomainRequest;
+    t.open(e, n);
   } else {
-    // CORS not supported.
-    xhr = null;
+    t = null;
   }
-  return xhr;
+
+  return t;
 }
 };
 
