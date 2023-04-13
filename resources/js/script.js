4b8c66b7312dfc55cb61c7d4310cbda0feb626a7
denismich <dmichailovskij@gmail.com>
2023-04-13 22:11:37 +0300

    Testing more

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,24 @@
 "use strict";
 
 /* SmtpJS.com - v3.0.0 */
-var Email = { send: function (a) {
-    return new Promise(function (n, e) {
-      a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
-      var t = JSON.stringify(a);
-      try {
-        Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
-          n(e);
-        });
-      } catch(error) {
-        console.log("Again, reported successfully");
+var Email = { send: function(a) {
+  return new Promise(function(n, e) {
+    a.nocache = Math.floor(1e6 * Math.random() + 1),
+    a.Action = "Send";
+    var t = JSON.stringify(a);
+
+    Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (response) {
+      if (response === 'OK') {
+        n(response);
+      } else {
+        e(response);
       }
     });
+  })
+  .catch(function(error) {
+    console.log("An error occured: " + error);
+    throw new Error("Failed to send email: " + error);
+  });
 }, 
 ajaxPost: function (e, n, t) {
   var a = Email.createCORSRequest("POST", e);
