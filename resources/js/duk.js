3b9a07ab11b811fcde8995934e02598cd2bff310
denismich <dmichailovskij@gmail.com>
2023-05-20 23:00:41 +0300

    Add faq aria

--- resources/js/duk.js
+++ resources/js/duk.js
@@ -7,10 +7,12 @@ for (var i = 0; i < faqToggles.length; i++) {
   (function(index) {
     faqToggles[index].addEventListener('click', function() {
       answers[index].classList.toggle('open');
+      answers[index].toggleAttribute(ariaHidden);
     });
     faqToggles[index].addEventListener('keydown', function(event) {
       if (event.keyCode === 13) {
         answers[index].classList.toggle('open');
+        answers[index].toggleAttribute(ariaHidden);
       }
     });
   })(i);
