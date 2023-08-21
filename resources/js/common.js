03ceebef7faea526cd81a60bc93600ad8983cc9d
denismich <dmichailovskij@gmail.com>
2023-08-21 23:37:33 +0300

    Optimize

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,45 +1,48 @@
 "use strict";
 
 /* SmtpJS.com - v3.0.0 */
-var Email = { send: function (a) {
-  return new Promise(function (n, e) {
-    a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
-    var t = JSON.stringify(a);
-    Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
-      n(e);
+var Email;
+if (typeof Calendly === 'undefined') {
+  Email = { send: function (a) {
+    return new Promise(function (n, e) {
+      a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
+      var t = JSON.stringify(a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
     });
-  });
-}, 
-ajaxPost: function (e, n, t) {
-  var a = Email.createCORSRequest("POST", e);
-  a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), 
-  a.onload = function () {
-    var e = a.responseText;
-    null != t && t(e);
+  }, 
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), 
+    a.onload = function () {
+      var e = a.responseText;
+      null != t && t(e);
+    },
+    a.onerror = function () {
+      oldBrowsersErrorModal.style.display = 'block';
+      window.scrollBy(0,-1);
+      oldBrowsersErrorModal.focus();
+    }, a.send(n);
   },
-  a.onerror = function () {
-    oldBrowsersErrorModal.style.display = 'block';
-    window.scrollBy(0,-1);
-    oldBrowsersErrorModal.focus();
-  }, a.send(n);
-},
-ajax: function (e, n) {
-  var t = Email.createCORSRequest("GET", e);
-  t.onload = function () {
-    var e = t.responseText;
-    null != n && n(e)
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e)
+    },
+    t.onerror = function () {
+      oldBrowsersErrorModal.style.display = 'block';
+      window.scrollBy(0,-1);
+      oldBrowsersErrorModal.focus();
+    }, t.send()
   },
-  t.onerror = function () {
-    oldBrowsersErrorModal.style.display = 'block';
-    window.scrollBy(0,-1);
-    oldBrowsersErrorModal.focus();
-  }, t.send()
-},
-createCORSRequest: function (e, n) {
-  var t = new XMLHttpRequest;
-  return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t;
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest;
+    return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t;
+  }
+  };
 }
-};
 
 //Fonts observer
 const openSansObserver = new FontFaceObserver('Open Sans');
