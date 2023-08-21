724dd12485a9266534e19d0ee1edcc2c55c59803
denismich <dmichailovskij@gmail.com>
2023-08-21 16:31:43 +0300

    test

---
+++ resources/js/test.js
@@ -0,0 +1,3 @@
+const preloadedScript = document.createElement("script");
+            preloadedScript.src = "https://use.fontawesome.com/e657b4494b.js";
+            document.head.appendChild(preloadedScript);
