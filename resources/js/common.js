3d5c6df0b17aceb14c5c89636547cc95044f7660
denismich <dmichailovskij@gmail.com>
2023-09-30 23:43:20 +0300

    Change email for test

--- resources/js/common.js
+++ resources/js/common.js
@@ -271,7 +271,7 @@ var sendEmail = function() {
 
   Email.send({
       SecureToken: '90677d85-8654-4537-91d0-eacde4cbff0d',
-      To : 'indre.ivanovaite@gmail.com',
+      To : 'dmichailovskij@gmail.com',
       From : 'dmichailovskij@gmail.com',
       Subject : 'Registracijos Procedūrai Užklausa',
       Body : notification
