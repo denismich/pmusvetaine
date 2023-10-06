8c83786073e482169776b571c46a1edf4e830fb6
denismich <dmichailovskij@gmail.com>
2023-10-06 23:22:41 +0300

    test

--- resources/js/apie.js
+++ resources/js/apie.js
@@ -1,49 +1,49 @@
 "use strict";
 
 //Variables to get the Firefox version from the userAgent string
-// var userAgent = navigator.userAgent;
-// var firefoxVersion = userAgent.match(/Firefox\/(\d+)/);
+var userAgent = navigator.userAgent;
+var firefoxVersion = userAgent.match(/Firefox\/(\d+)/);
 
-// //Header height for scrolling/links to elements
-// var headerHeight = document.querySelector('header').clientHeight;
+//Header height for scrolling/links to elements
+var headerHeight = document.querySelector('header').clientHeight;
 
-// //Banner registration button
-// var registrationSection = document.querySelector('#registration-section-old');
-// var bannerRegistrationButton = document.querySelector('#banner-registration-button');
-// var initCalendlyPopupWidget = function() {
-//   Calendly.initPopupWidget({url: 'https://calendly.com/indre-ivanovaite?hide_landing_page_details=1&hide_gdpr_banner=1'});
-//   return false;
-// }
+//Banner registration button
+var registrationSection = document.querySelector('#registration-section-old');
+var bannerRegistrationButton = document.querySelector('#banner-registration-button');
+var initCalendlyPopupWidget = function() {
+  Calendly.initPopupWidget({url: 'https://calendly.com/indre-ivanovaite?hide_landing_page_details=1&hide_gdpr_banner=1'});
+  return false;
+}
 
-// bannerRegistrationButton.addEventListener('click', initCalendlyPopupWidget);
+bannerRegistrationButton.addEventListener('click', initCalendlyPopupWidget);
 
-// window.addEventListener('load', function() {
-//   if (typeof Calendly === 'undefined' || (firefoxVersion && firefoxVersion[1] >= 72 && firefoxVersion[1] <= 73)) {
-//     bannerRegistrationButton.removeEventListener('click', initCalendlyPopupWidget);
-//     bannerRegistrationButton.addEventListener('click', function() {
-//       window.scrollBy(0, registrationSection.getBoundingClientRect().top - headerHeight);
-//       setTimeout(function() {
-//         document.querySelector('#name').focus();
-//         window.scrollBy(0, registrationSection.getBoundingClientRect().top - headerHeight);
-//       }, 100);
-//     });
-//   }
-// });
+window.addEventListener('load', function() {
+  if (typeof Calendly === 'undefined' || (firefoxVersion && firefoxVersion[1] >= 72 && firefoxVersion[1] <= 73)) {
+    bannerRegistrationButton.removeEventListener('click', initCalendlyPopupWidget);
+    bannerRegistrationButton.addEventListener('click', function() {
+      window.scrollBy(0, registrationSection.getBoundingClientRect().top - headerHeight);
+      setTimeout(function() {
+        document.querySelector('#name').focus();
+        window.scrollBy(0, registrationSection.getBoundingClientRect().top - headerHeight);
+      }, 100);
+    });
+  }
+});
 
-// //Handling about me photo height
-// var aboutMePhoto = document.querySelector('.about-me-photo img');
-// var resizeAboutMePhoto = function() {
-//   aboutMePhoto.classList.remove('with-max-height');
-//   aboutMePhoto.classList.remove('with-max-width');
-//   if (aboutMePhoto.clientHeight > document.documentElement.clientHeight * 0.8) {
-//     aboutMePhoto.classList.add('with-max-height');
-//   }
-//   if (document.documentElement.clientWidth > 900 && aboutMePhoto.clientWidth > document.documentElement.clientWidth * 0.35) {
-//     aboutMePhoto.classList.add('with-max-width');
-//   }
-// };
-// window.addEventListener('load', resizeAboutMePhoto);
-// window.addEventListener('resize', resizeAboutMePhoto);
+//Handling about me photo height
+var aboutMePhoto = document.querySelector('.about-me-photo img');
+var resizeAboutMePhoto = function() {
+  aboutMePhoto.classList.remove('with-max-height');
+  aboutMePhoto.classList.remove('with-max-width');
+  if (aboutMePhoto.clientHeight > document.documentElement.clientHeight * 0.8) {
+    aboutMePhoto.classList.add('with-max-height');
+  }
+  if (document.documentElement.clientWidth > 900 && aboutMePhoto.clientWidth > document.documentElement.clientWidth * 0.35) {
+    aboutMePhoto.classList.add('with-max-width');
+  }
+};
+window.addEventListener('load', resizeAboutMePhoto);
+window.addEventListener('resize', resizeAboutMePhoto);
 
 //Gallery images enlarging
 var imageContainer = document.querySelector('#gallery-photos-container');
@@ -159,111 +159,111 @@ nextButton.addEventListener('click', function() {
   fullScreenImage.src = imageArray[currentImageIndex].children[2].src.replace('/gallery/', '/gallery-enlarged/');
 });
 
-//Testimonials slides
-var slideshowContainer = document.querySelector('#slideshow-container');
-var slidesContainerWidth = slideshowContainer.clientWidth;
-var dots = document.getElementsByClassName('dot');
+// //Testimonials slides
+// var slideshowContainer = document.querySelector('#slideshow-container');
+// var slidesContainerWidth = slideshowContainer.clientWidth;
+// var dots = document.getElementsByClassName('dot');
 
-var slideIndex = 1;
+// var slideIndex = 1;
 
-var showSlides = function(n) {
-  var slides = document.getElementsByClassName('slide');
+// var showSlides = function(n) {
+//   var slides = document.getElementsByClassName('slide');
   
-  n > slides.length && (slideIndex = 1);
-  n < 1 && (slideIndex = slides.length);
+//   n > slides.length && (slideIndex = 1);
+//   n < 1 && (slideIndex = slides.length);
 
-  for (var i = 0; i < slides.length; i++) {
-      slides[i].style.transform = `translateX(${(i+slideIndex-1) * 100}%)`;
-      dots[i].classList.remove('active');
-  }
-  slides[slideIndex-1].style.transform = 'translateX(0)';
-  dots[slideIndex-1].className += ' active';
-}
+//   for (var i = 0; i < slides.length; i++) {
+//       slides[i].style.transform = `translateX(${(i+slideIndex-1) * 100}%)`;
+//       dots[i].classList.remove('active');
+//   }
+//   slides[slideIndex-1].style.transform = 'translateX(0)';
+//   dots[slideIndex-1].className += ' active';
+// }
 
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
-var myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000);
+// var myTimer = setInterval(function() {showSlides(slideIndex += 1)}, 4000);
 
-var pause = function() {clearInterval(myTimer)};
+// var pause = function() {clearInterval(myTimer)};
 
-var touchStartX;
-var touchEndX;
-var deltaX;
-var slidesTouchStartInContainer;
-var touchStartsCount = 0;
-var maxTouchStartsCount = 0;
+// var touchStartX;
+// var touchEndX;
+// var deltaX;
+// var slidesTouchStartInContainer;
+// var touchStartsCount = 0;
+// var maxTouchStartsCount = 0;
 
-window.addEventListener('touchstart', function() {
-  touchStartsCount += 1;
-  maxTouchStartsCount += 1;
-} ,passiveArgument)
+// window.addEventListener('touchstart', function() {
+//   touchStartsCount += 1;
+//   maxTouchStartsCount += 1;
+// } ,passiveArgument)
 
-window.addEventListener('touchend', function() {
-  setTimeout(function() {
-    if (touchStartsCount === 1) {
-      maxTouchStartsCount = 0;
-    }
-    touchStartsCount -= 1;
-  }, 100);
-} ,passiveArgument)
+// window.addEventListener('touchend', function() {
+//   setTimeout(function() {
+//     if (touchStartsCount === 1) {
+//       maxTouchStartsCount = 0;
+//     }
+//     touchStartsCount -= 1;
+//   }, 100);
+// } ,passiveArgument)
 
-window.addEventListener('DOMContentLoaded', function() {
-  slideshowContainer.style.visibility = 'visible';
-  slideshowContainer.addEventListener('click', pause);
-  slideshowContainer.addEventListener('mousedown', function(event) {
-    slidesTouchStartInContainer = true;
-    touchStartX = event.pageX;
-  }, passiveArgument);
-  slideshowContainer.addEventListener('mouseup', function(event) {
-    touchEndX = event.pageX;
-    deltaX = touchEndX - touchStartX;
-    if (slidesTouchStartInContainer === true) {
-      slidesTouchStartInContainer = null;
-      if (deltaX <= slidesContainerWidth * -0.20) {
-        showSlides(slideIndex += 1);
-        clearInterval(myTimer);
-      } else if (deltaX >= slidesContainerWidth * 0.20) {
-        showSlides(slideIndex -= 1);
-        clearInterval(myTimer);
-      }
-    }
-  }, passiveArgument);
-  slideshowContainer.addEventListener('touch', pause, passiveArgument);
-  slideshowContainer.addEventListener('touchstart', function(event) {
-    slidesTouchStartInContainer = true;
-    touchStartX = event.changedTouches[0].pageX;
-  }, passiveArgument);
-  slideshowContainer.addEventListener('touchend', function(event) {
-    if (maxTouchStartsCount === 1) {
-      touchEndX = event.changedTouches[0].pageX;
-      deltaX = touchEndX - touchStartX;
-      if (slidesTouchStartInContainer === true) {
-        slidesTouchStartInContainer = null;
-        if (deltaX <= slidesContainerWidth * -0.25) {
-          showSlides(slideIndex += 1);
-          clearInterval(myTimer);
-        } else if (deltaX >= slidesContainerWidth * 0.25) {
-          showSlides(slideIndex -= 1);
-          clearInterval(myTimer);
-        }
-      }
-    }
-  }, passiveArgument);
-});
+// window.addEventListener('DOMContentLoaded', function() {
+//   slideshowContainer.style.visibility = 'visible';
+//   slideshowContainer.addEventListener('click', pause);
+//   slideshowContainer.addEventListener('mousedown', function(event) {
+//     slidesTouchStartInContainer = true;
+//     touchStartX = event.pageX;
+//   }, passiveArgument);
+//   slideshowContainer.addEventListener('mouseup', function(event) {
+//     touchEndX = event.pageX;
+//     deltaX = touchEndX - touchStartX;
+//     if (slidesTouchStartInContainer === true) {
+//       slidesTouchStartInContainer = null;
+//       if (deltaX <= slidesContainerWidth * -0.20) {
+//         showSlides(slideIndex += 1);
+//         clearInterval(myTimer);
+//       } else if (deltaX >= slidesContainerWidth * 0.20) {
+//         showSlides(slideIndex -= 1);
+//         clearInterval(myTimer);
+//       }
+//     }
+//   }, passiveArgument);
+//   slideshowContainer.addEventListener('touch', pause, passiveArgument);
+//   slideshowContainer.addEventListener('touchstart', function(event) {
+//     slidesTouchStartInContainer = true;
+//     touchStartX = event.changedTouches[0].pageX;
+//   }, passiveArgument);
+//   slideshowContainer.addEventListener('touchend', function(event) {
+//     if (maxTouchStartsCount === 1) {
+//       touchEndX = event.changedTouches[0].pageX;
+//       deltaX = touchEndX - touchStartX;
+//       if (slidesTouchStartInContainer === true) {
+//         slidesTouchStartInContainer = null;
+//         if (deltaX <= slidesContainerWidth * -0.25) {
+//           showSlides(slideIndex += 1);
+//           clearInterval(myTimer);
+//         } else if (deltaX >= slidesContainerWidth * 0.25) {
+//           showSlides(slideIndex -= 1);
+//           clearInterval(myTimer);
+//         }
+//       }
+//     }
+//   }, passiveArgument);
+// });
 
-for (var i = 0; i < dots.length; i++) {
-  (function(index) {
-    dots[index].addEventListener('click', function() {
-      showSlides(slideIndex = index + 1);
-      clearInterval(myTimer);
-    });
-    dots[index].addEventListener('keydown', function(event) {
-      if (event[keyCode] === 13) {
-        dots[index].click();
-      }
-    });
-  })(i);
-}
+// for (var i = 0; i < dots.length; i++) {
+//   (function(index) {
+//     dots[index].addEventListener('click', function() {
+//       showSlides(slideIndex = index + 1);
+//       clearInterval(myTimer);
+//     });
+//     dots[index].addEventListener('keydown', function(event) {
+//       if (event[keyCode] === 13) {
+//         dots[index].click();
+//       }
+//     });
+//   })(i);
+// }
 
 //Adjusting testimonials section position when redirected from links
 window.addEventListener('DOMContentLoaded', function() {
